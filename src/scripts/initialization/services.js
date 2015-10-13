099f6e71d5a1d520429d44bb158062aa3b861e5a
Simon <simonxd@gmail.com>
2015-10-13 17:18:20 -0300

    basic i18next setup

--- src/scripts/initialization/services.js
+++ src/scripts/initialization/services.js
@@ -9,11 +9,14 @@
 var LocalStorageAPI          = require('../services/local-storage');
 var UserServiceClient        = require('../services/user');
 var ProjectsServiceClient    = require('../services/projects');
+var I18NService              = require('../services/i18n');
 
 /**
  * Export function that receives carbo and configurations
  */
 module.exports = function (carbo, config) {
+
+    var i18nService = new I18NService();
 
     // localstorage service
     var localStorage = new LocalStorageAPI(config);
@@ -35,6 +38,7 @@ module.exports = function (carbo, config) {
     carbo.set('services', {});
     carbo.set('services.userService', userService);
     carbo.set('services.projectsService', projectsService);
+    carbo.set('services.i18nService', i18nService);
 
     // return all services as a result of the initialization
     return carbo.get('services');
