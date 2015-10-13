c60a93913492b73d2d1bffa5d44f92fbdf5d6aa4
Aline Takechi <akt@cesar.org.br>
2015-10-13 17:30:51 -0300

    [JRD-487] structure for create project component

---
+++ src/elements/project-page/project-page.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+    // Register the element with Polymer
+    Polymer({
+        is: 'carbo-project-page',
+
+        properties: {
+            projects: {
+                type: Array,
+                notify: true
+            }
+        }
+    });
+})();
