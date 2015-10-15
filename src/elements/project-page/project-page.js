dc22a7ffc52224ca20ddac7adf2c2962512363a8
Vitor Casadei <vitor@casadei.io>
2015-10-15 13:31:11 -0300

    [JRD-550] - Fields Validation - Error messages - Redirects

--- src/elements/project-page/project-page.js
+++ src/elements/project-page/project-page.js
@@ -1,6 +1,5 @@
 (function (window) {
     'use strict';
-
     // Register the element with Polymer
     Polymer({
         is: 'carbo-project-page',
@@ -11,81 +10,96 @@
                 notify: true
             }
         },
+        ready: function () {
+            var PROJECT_NAME_MAX_LENGTH = 60;
+            var submitButton = this.$.createProjectButton;
+            var projectNameInput = this.$.projectName;
+            projectNameInput.addEventListener("keyup", validateProjectName);
+            projectNameInput.addEventListener("focusout", validateProjectName);
+            submitButton.disabled = true;
+            // project name beggins invalid
+            var validName = false;
+            
+            // function to enable/disable submit-button
+            function enableDisableSubmitButton() {
+                // check if input validations are true
+                if (validName) {
+                    submitButton.disabled = false;
+                } else {
+                    submitButton.disabled = true;
+                }
+
+            }
+            
+            // validates Project Name
+            function validateProjectName() {
+                var value = projectNameInput.value;
+                if (value === "" || value.length > PROJECT_NAME_MAX_LENGTH) {
+                    validName = false;
+                    submitButton.disabled = true;
+                    projectNameInput.parentElement.className += " error";
+                } else {
+                    validName = true;
+                    projectNameInput.parentElement.className = "input-container";
+                }
+                enableDisableSubmitButton();
+            }
+        },
         openCreatePopup: function() {
+            // Init
+            this.$.projectName.value = '';
+            this.$.projectName.parentElement.className = "input-container";
+            this.$.projectDescription.value = '';
+            this.$.createProjectButton.disabled = false;
+            // Open
             this.$.createPopup.open();
         },
         submitCreateProject: function (e) {
-            
-            var validateProjectName =  function (projName) {
-                var error = [];
-                if (projName === undefined || projName === null) {
-                    error.push({
-                        field: 'name',
-                        error: 'name is required',
-                    });
-                } else {
-                    if (projName.length > 60) {
-                        error.push({
-                            field: 'name',
-                            error: 'name must not have more than 60 chars',
-                        });
-                    }
-                }
-
-                return error;
+            var project = {
+                name: this.newProjName,
+                description: this.newProjDescription || "",
             };
-            var error = null;
-            error = validateProjectName(this.newProjName);
-            if (error !== null && error.length === 0) {
-                var project = {
-                    name: this.newProjName,
-                    description: this.newProjDescription || "",
-                };
-                var popup = this.$.createPopup;
-                var userService = window.services.userService;
-                var projectsService = window.services.projectsService;
-                var redirectService = window.services.redirectService;
-                var carbo = window.carbo;
-                // check if user is logged
-                userService.getLoggedUserData()
-                    .then(function (userData) {
-                        carbo.set('userData', userData);
+            var popup = this.$.createPopup;
+            var userService = window.services.userService;
+            var projectsService = window.services.projectsService;
+            var redirectService = window.services.redirectService;
+            var carbo = window.carbo;
+            // check if user is logged
+            userService.getLoggedUserData()
+                .then(function (userData) {
+                    carbo.set('userData', userData);
 
-                        // create Project
-                        return projectsService.create({
-                            email: userData.email,
-                            name: project.name,
-                            description: project.description,
-                        });
+                    // create Project
+                    return projectsService.create({
+                        email: userData.email,
+                        name: project.name,
+                        description: project.description,
+                    });
 
-                    }, function (err) {
-                        // user not logged
+                }, function (err) {
+                    // user not logged
+                    alert('Sessão expirada, por favor, faça login novamente');
+                    popup.close();
+                    redirectService.redirectLogin();
+                })
+                .then(function (projectCreated) {
+                    alert('Projeto criado com sucesso!');
+                    popup.close();
+                    redirectService.redirectProjects();
+                    // Message and page redirect
+                }, function (err) {
+                    if (err.code === 403) {
                         alert('Sessão expirada, por favor, faça login novamente');
                         popup.close();
                         redirectService.redirectLogin();
-                    })
-                    .then(function (projectCreated) {
-                        alert('Projeto criado com sucesso!');
+                    } else {
+                        alert('Erro ao criar projeto!');
                         popup.close();
                         redirectService.redirectProjects();
-                        // Message and page redirect
-                    }, function (err) {
-                        if (err.code === 403) {
-                            alert('Sessão expirada, por favor, faça login novamente');
-                            popup.close();
-                            redirectService.redirectLogin();
-                        } else {
-                            alert('Erro ao criar projeto!');
-                            popup.close();
-                            redirectService.redirectProjects();
-                        }
                     }
-                )
-                    .done();
-            } else {
-                alert(error);
-            }
-            
+                }
+            )
+                .done();
         }
     });
 
---
+++ src/scripts/validation.js
@@ -0,0 +1,11 @@
+'use strict';
+
+(function() {
+
+    // CONSTANTS
+    var PROJECT_NAME_MAX_LENGTH     = 60;
+
+    var projectCreationForm = document.getElementById('createPopup');
+    
+    
+})();
